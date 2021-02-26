c10ddaa6e7f35d2f88ad63984b17c6bc19b2fd90
juansvelasco22 <juseveco@gmail.com>
2021-02-26 11:13:37 -0500

    Funciones

--- Control Structures.R
+++ Control Structures.R
@@ -47,3 +47,75 @@ for(i in seq_len(nrow(x))){
     print(x[i,j])
   }
 }
+
+
+#While
+
+#Una sola condicion
+count<-0
+while(count<10){
+  print(count)
+  count<-count+1
+}
+
+#Dos o mas condiciones
+z<-5
+
+while(z>=3 && z<=10){
+  print(z)
+  coin<- rbinom(1,1,0.5)
+  
+  
+  if(coin==1){     ## Random walk
+    
+    z <- z + 1
+    
+  } else{
+    
+    z <- z - 1
+    
+  }
+}
+
+#Repeat, Next, Break
+# REPEAT inicia un loop infinito, este loop solo se para con BREAK
+
+x0<-1
+tol<- 1e-8
+
+repeat{
+  x1<- computeEstimate() #Funcion que estima valores / Funcion objetivo
+  
+  if(abs(x1-x0)<tol){
+    break
+  } else{
+    x0<-x1
+  }
+}
+
+#Functions
+
+add2 <- function(x,y){
+  x+y
+}
+
+above10 <- function(x){
+  use<- x>10
+  x[use]
+}
+
+above <- function(x, n=10){ ## Se especifica un valor en caso de que no se entre un argumento en n 
+  use <- x > n
+  x[use]
+}
+
+colum_mean <- function(m, removeNA = TRUE){ ## Variable logica para eliminar valores NaN 
+  nc <- ncol(m)
+  means <- numeric(nc)  ## Inicializando el vector que guarda los promedios
+  
+  for(i in 1:nc){
+    means[i] <- mean(m[,i], na.rm=removeNA) ## Especifico a la funcion mean() que elimine los NaN, que no los tenga encuenta
+  }
+  means
+}
+
